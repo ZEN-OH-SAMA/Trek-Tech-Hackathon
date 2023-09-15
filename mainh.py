2f27aaeb7dec2c7319a9b54a084fb4c4f79c0748
balram16 <141282145+balram16@users.noreply.github.com>
2023-09-15 14:23:32 +0530

    Add files via upload

---
+++ mainh.py
@@ -0,0 +1,8 @@
+print("Welcome to the college Events Information Section")
+print("We have number of events organised by the college")
+print("Some of the Events organised by us are")
+print("1.The Grant Event  Zephrs\n2.Freshers for the new students\n3.Various department level Fests by Every Department\n3.Hackathons\n4.Games Competition\n5.Varioius Events on Festivals\n6.Orientation programs"
+      "7.Tantragyan\n8.Robocon\n9.Seminars\n10.Event Calender")
+
+
+
